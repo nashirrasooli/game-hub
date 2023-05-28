31cec0d0ad62c6fb2a563c4825a4fd88c025080a
nashirrasooli <nashir.rasooli1@gmail.com>
2023-05-28 10:30:29 +0430

    Building sort selector

--- React-Projects/game-hub/src/services/apiClient.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.rawg.io/api',
-  params: {
-    key: 'af2a4598e081420cb9edf9b978d91c5c',
-  },
-});
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,24 +1,24 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
-import useGames, { Platform } from '../hooks/useGames';
+import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import LoadingSkeleton from './LoadingSkeleton';
 import GameCardContainer from './GameCardContainer';
-import { Genre } from '../hooks/useGenres';
+
+import { GameQuery } from '../App';
 
 interface Props {
-  selectedGenre: Genre | null;
-  selectedPlaftform: Platform | null;
+  gameQuery: GameQuery;
 }
 
-const GameGrid = ({ selectedGenre, selectedPlaftform }: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre, selectedPlaftform);
+const GameGrid = ({ gameQuery }: Props) => {
+  const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding='10px'
+        padding="10px"
         spacing={3}
       >
         {isLoading &&
